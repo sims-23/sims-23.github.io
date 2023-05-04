12eec315b881ce8422560cc26270ecb73623831c
sims-23 <simsanghera.23@gmail.com>
2023-05-04 11:14:36 -0700

    updated content and made website a bit more job ready.

--- js/app.js
+++ js/app.js
@@ -15,3 +15,29 @@ const body = document.querySelector("body");
     window.onscroll = ()=>{
       this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
     }
+
+// future problem: add recent blog posts
+// const blogFolder = "./blog"; // Change this to the path of your blog folder
+// const numPostsToShow = 1; // Change this to the number of recent posts to display
+
+// fetch(blogFolder)
+//   .then((response) => response.text())
+//   .then((html) => {
+//     const parser = new DOMParser();
+//     const doc = parser.parseFromString(html, "text/html");
+//     const links = Array.from(doc.querySelectorAll("a"));
+//     const sortedLinks = links
+//       .filter((link) => link.href.endsWith(".html")) // Change this if your blog posts have a different file extension
+//       .sort((a, b) => b.lastModified - a.lastModified) // Sort by last modified date, newest first
+//       .slice(0, numPostsToShow); // Only show the most recent posts
+
+//     const blogPostsList = document.getElementById("blog-posts");
+//     sortedLinks.forEach((link) => {
+//       const listItem = document.createElement("li");
+//       const anchor = document.createElement("a");
+//       anchor.href = link.href;
+//       anchor.innerText = link.innerText;
+//       listItem.appendChild(anchor);
+//       blogPostsList.appendChild(listItem);
+//     });
+//   });
